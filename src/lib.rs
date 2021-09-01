1d757fcfb487f902a550aed159d7f21eafe9cbb7
iqbigbang <iqbigbang@gmail.com>
2021-09-01 14:32:30 +0200

    fix: cosmetic changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod ty;
 pub mod instr;
-pub mod metadata;
+// This module doesn't need to be public as it doesn't contain anything public anyway
+pub(crate) mod metadata;
 pub mod module;
 pub mod pass;
 pub mod verify;
@@ -11,3 +12,20 @@ pub mod correct;
 pub mod cf_verify;
 pub mod abi;
 pub mod passes;
+
+/// Compile an IR Module to WebAssembly with the default
+/// preferred pipeline.
+///
+/// This is a simplification for users so that they don't have
+/// to worry about invoking necessary passes in correct order.
+///
+/// Panics if any kind of error happens while compiling/verifying etc.
+pub fn pipeline_compile_module_to_wasm(mut module: module::Module<'_>) -> Vec<u8> {
+    module.do_mut_pass(&mut correct::CorrectionPass{}).unwrap();
+    module.do_mut_pass(&mut cf_verify::ControlFlowVerifier{}).unwrap();
+    module.do_mut_pass(&mut verify::Verifier{}).unwrap();
+
+    let mut e: emit::WasmEmitter<abi::Wasm32Abi> = emit::WasmEmitter::new();
+    module.do_pass(&mut e).unwrap();
+    e.finish()
+}
